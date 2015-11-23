30b4325871d82bb60a467196f3f685d76ee5290b
paulthinkbox <paul@thinkbox.us>
2015-11-23 09:46:51 -0500

    begin

---
+++ JIRA-PurgeProjectComponents/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace JIRA_PurgeProjectComponents
+{
+    class Program
+    {
+        public static string _URI;
+        public static string _Project;
+        public static string _UserName;
+        public static string _Password;
+
+        static void Main(string[] args)
+        {
+            // enable this if you need to break the command-line and attach debugger
+            //Console.ReadLine();
+
+            if (args.Length != 4)
+            {
+                System.Console.WriteLine("Usage: JIRA-ClearAllComponents <UserName> <Password> <JIRA_REST_BASE_URI> <PROJECT>");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    _UserName = args[0].ToString();
+                    _Password = args[1].ToString();
+                    _URI = args[2].ToString();
+                    _Project = args[3].ToString();
+                }
+                catch
+                {
+                    System.Console.WriteLine("Check the Parameters");
+                }
+            }
+
+            Action<JObject[]> doc = delegate (JObject[] d) { ProcessXML(d); };
+            Action<Exception> err = delegate (Exception e) { DealWithError(e); };
+
+            RestService r = new RestService();
+            r.MakeJsonRequest<JObject[]>(_URI, doc, err);
+        }
+
+        private static void ProcessXML(JObject[] message)
+        {
+            RestService r = new RestService();
+
+            foreach (JObject j in message)
+            {
+                r.MakeDeleteRequest(_URI, (string)j["id"]);
+            }
+        }
+
+        private static void DealWithError(Exception x)
+        {
+            Console.WriteLine(x.Message);
+        }
+
+    }
+}
---
+++ JIRA-PurgeProjectComponents/restservice.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JIRA_PurgeProjectComponents
+{
+
+    public class RestService
+    {
+
+        public void MakeXmlRequest<T>(string uri, Action<XmlDocument> successAction, Action<Exception> errorAction)
+        {
+            XmlDocument XMLResponse = new XmlDocument();
+
+            HttpWebRequest request = PrepGetRequest();
+            string doc = "";
+            MakeRequest(request, response => doc = response,
+                                (error) =>
+                                {
+                                    if (errorAction != null)
+                                    {
+                                        errorAction(error);
+                                    }
+                                }
+                       );
+            XMLResponse.LoadXml(doc);
+            successAction(XMLResponse);
+        }
+
+        public void MakeJsonRequest<T>(string uri, Action<T> successAction, Action<Exception> errorAction)
+        {
+            HttpWebRequest request = PrepGetRequest();
+
+            MakeRequest(
+               request,
+               (response) =>
+               {
+                   if (successAction != null)
+                   {
+                       T toReturn;
+                       try
+                       {
+                           toReturn = Deserialize<T>(response);
+                       }
+                       catch (Exception ex)
+                       {
+                           errorAction(ex);
+                           return;
+                       }
+                      successAction(toReturn);
+                   }
+               },
+               (error) =>
+               {
+                   if (errorAction != null)
+                   {
+                       errorAction(error);
+                   }
+               }
+            );
+        }
+
+        private HttpWebRequest PrepGetRequest()
+        {
+            StringBuilder url = new StringBuilder();
+            url.Append(Program._URI);
+            url.Append(String.Format("/rest/api/2/project/{0}/components", Program._Project));
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url.ToString());
+            string authInfo = Program._UserName + ":" + Program._Password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Timeout = 30000;
+            request.KeepAlive = false;
+
+            request.Headers["Authorization"] = "Basic " + authInfo;
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            return request;
+        }
+
+        public void MakeDeleteRequest(string uri, string idToDelete)
+        {
+            XmlDocument XMLResponse = new XmlDocument();
+
+            string authInfo = Program._UserName + ":" + Program._Password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authInfo);
+                var response = client.DeleteAsync(String.Format("/rest/api/2/component/{0}", idToDelete)).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(String.Format("Successfully Removed: {0}", idToDelete));
+                }
+                else
+                    Console.WriteLine(String.Format("Failed to Remove: {0}", idToDelete));
+            }
+        }
+
+        private void MakeRequest(HttpWebRequest request, Action<string> successAction, Action<Exception> errorAction)
+        {
+            try
+            {
+                using (var webResponse = (HttpWebResponse)request.GetResponse())
+                {
+                    using (var reader = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        var objText = reader.ReadToEnd();
+                        successAction(objText);
+                    }
+                }
+            }
+            catch (HttpException ex)
+            {
+                errorAction(ex);
+            }
+        }
+        private T Deserialize<T>(string responseBody)
+        {
+            try
+            {
+                var toReturns = JsonConvert.DeserializeObject<T>(responseBody);
+                return toReturns;
+            }
+            catch (Exception ex)
+            {
+                string errores;
+                errores = ex.Message;
+            }
+            var toReturn = JsonConvert.DeserializeObject<T>(responseBody);
+            return toReturn;
+        }
+    }
+}
+
